aaedbc1b498ea8e7259138726dba9dfe13086fa6
nelson <nelsonplanes@gmail.com>
2026-02-19 09:04:39 -0400

    Fix dual React instances: remove npm workspace hoisting, add wrangler.toml to build from frontend dir

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,15 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      react: path.resolve(__dirname, 'node_modules/react'),
-      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
-      'styled-jsx': path.resolve(__dirname, 'node_modules/styled-jsx'),
-    };
-    return config;
-  },
 };
 
 module.exports = nextConfig;
